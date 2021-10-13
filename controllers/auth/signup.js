106124d5f60bb5e82cefafa7635bdb43ab2ff97a
Alina Akompi <alina.akompi@gmail.com>
2021-10-13 20:57:01 +0300

    Register + auth middleware

---
+++ controllers/auth/signup.js
@@ -0,0 +1,32 @@
+const { User } = require("../../models/user");
+const { sendResponse } = require("../../helpers");
+
+const signup = async (req, res) => {
+  const { email, password } = req.body;
+  const result = await User.findOne({ email });
+  if (result) {
+    sendResponse({
+      res,
+      status: 409,
+      statusMessage: "error",
+      data: { message: "Email in use" },
+    });
+    return;
+  }
+
+  const newUser = new User({ email });
+  newUser.setPassword(password);
+  await newUser.save();
+
+  sendResponse({
+    res,
+    status: 201,
+    data: {
+      user: {
+        email: newUser.email,
+      },
+    },
+  });
+};
+
+module.exports = signup;
