bd02c47f955a44a908be68942b0e850270d18abf
Gupanets Dmytro <dmgpnts@gmail.com>
2021-10-17 12:21:45 +0300

    fix for books controllers

--- controllers/books/add.js
+++ controllers/books/add.js
@@ -4,9 +4,8 @@ const { sendResponse } = require("../../helpers");
 const add = async (req, res) => {
   const newBook = { ...req.body, owner: req.user._id };
   const book = await Book.create(newBook);
-  await book.populate("owner", "_id");
 
-  sendResponse({ res, data: { book }, status: 201 });
+  return sendResponse({ res, data: { book }, status: 201 });
 };
 
 module.exports = add;
