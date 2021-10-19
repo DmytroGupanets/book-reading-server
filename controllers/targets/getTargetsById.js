6ad50211c68578edccdbb7d1845e2c4ced97bad2
Gupanets Dmytro <dmgpnts@gmail.com>
2021-10-19 23:07:06 +0300

    remove console logs

--- controllers/targets/getTargetsById.js
+++ controllers/targets/getTargetsById.js
@@ -4,8 +4,6 @@ const { sendResponse } = require("../../helpers");
 const getTargetById = async (req, res) => {
   const { id } = req.params;
   const result = await Target.findOne({ owner: id });
-  console.log(result);
-  console.log(req.params);
   if (!result) {
     return sendResponse({
       res,
